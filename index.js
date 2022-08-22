36147630e57f10d464cc12d747e6eeecae80ac4b
Riyadhkurn <mriyadhkurniawan@gmail.com>
2022-08-23 00:07:43 +0700

    3. Belajar JS

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log("hello worlds!")
+// console.log("hello worlds!")
+let namaLengkap= "Gracia P"
+const tempatLahir= "Bandung"
+const umur= 21
+const tinggiBadan= 140
+const boybandFav= ["BTS", "SUJU", "EXO", "SMASH"]
+const isChindo= false
+const isJavanese= true
+
+// console.log(`Pacar saya ${namaLengkap}`);
+// console.log(`Lahir di ${tempatLahir}`);
+// console.log(`Umur ${umur} jadi sudah legal`);
+// console.log(`tinggi badan ${tinggiBadan}, jika tinggi badan + umur adalah ${tinggiBadan + umur}`);
+// console.log(`dia paling suka boyband ${boybandFav[0]}`);
+// if (isChindo==true) {
+//     console.log("bikin sange")
+// } else {
+//     console.log("kurang")
+// }
+// if (isChindo==true) {
+//     console.log("bikin sange")
+// } else if (isJavanese==true) {
+//     console.log("manis")
+// } else {
+//     console.log("TIDAKKKKK");
+// }
+
+// console.log(boybandFav);
+// boybandFav[3]= "Blackpink"
+// console.log(boybandFav);
+// boybandFav= "cauk"
+// console.log(boybandFav);
+
+const kenalan=(namaCewek)=> {
+    console.log(`Halo ${namaCewek}, ujar ${namaLengkap}`)
+}
+// kenalan("fir")
+
+const namaTokoh=["Jena", "Grace", "Elis", "Tya", "Risa", "Cauk"]
+for (let index = 0; index < namaTokoh.length; index++) {
+    // console.log(namaTokoh[index])
+    // console.log("sudah di ews")
+    kenalan(namaTokoh[index])
+}
